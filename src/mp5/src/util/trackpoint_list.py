2be4444250aefccfd472c65aa36a1f670b4690a6
kris miller <kmmille2@illinois.edu>
2021-01-11 22:19:15 -0600

    webpage initial commit

--- src/mp5/src/util/trackpoint_list.py
+++
@@ -1,9 +0,0 @@
-import numpy as np    
-import pickle
-
-class TrackPoints():
-    def __init__(self):
-        self.trackpts = pickle.load(open('../waypoints', 'rb'))
-
-    def getWayPoints(self):
-        return self.trackpts
