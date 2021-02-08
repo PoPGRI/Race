b61f268bb6998c88f0bd1b3fb81b17700b7e3227
sundw2014 <sundw2014@gmail.com>
2021-02-07 22:12:02 -0600

    use json template

--- src/race/src/run.py
+++ src/race/src/run.py
@@ -8,7 +8,7 @@ import os, signal
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('-N', type=int, default='3')
-parser.add_argument('--log', type=str)
+parser.add_argument('--log', type=str, default='/tmp/')
 
 args = parser.parse_args()
 
@@ -16,22 +16,35 @@ vehicles = []
 for i in range(args.N):
     # in .launch file: spawn vehicle, control bridge, perception module
     v = {}
+
     v['launch_log'] = args.log+'/vehicle-%d_launch_log.txt'%i
+
     role_name = 'vehicle-%d'%i
     v['role_name'] = role_name
-    init_pose = []
+
+    with open('objects.json.template', 'r') as f:
+        obj = f.read()
+    obj = obj.replace('[[role_name]]', role_name)
+    json_file = '/tmp/objects_%s.json'%role_name
+    v['json_file'] = json_file
+
+    with open(v['json_file'], 'w') as f:
+        f.write(obj)
+
+    init_pose = [-17+i*5,-132,0,0,0,180]
     v['init_pose'] = init_pose
-    cmd = ('roslaunch spawn_vehicle.launch role_name:=%s spawn_point:=' + ','.join(['%f',]*6) + ' &> %s')%tuple([v['role_name'],]+*(v['init_pose'])+[v['launch_log'],])
+
+    cmd = ('roslaunch spawn_vehicle.launch config_file:=%s role_name:=%s spawn_point:=' + ','.join(['%f',]*6) + ' &> %s')%tuple([v['json_file'], v['role_name'],]+*(v['init_pose'])+[v['launch_log'],])
     # The os.setsid() is passed in the argument preexec_fn so
     # it's run after the fork() and before  exec() to run the shell.
     v['proc_handler'] = subprocess.Popen(cmd, preexec_fn=os.setsid, shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
-rospy.init_node('RaceMain')
-rate = rospy.Rate(100)  # 100 Hz    
+# rospy.init_node('RaceMain')
+# rate = rospy.Rate(100)  # 100 Hz    
 
-# subscribe to positions
-while not rospy.is_shutdown():
-    rate.sleep()
+# # subscribe to positions
+# while not rospy.is_shutdown():
+#     rate.sleep()
 
-for i in range(args.N):
-    os.killpg(os.getpgid(v['proc_handler'].pid), signal.SIGTERM)  # Send the signal to all the process groups
+# for i in range(args.N):
+#     os.killpg(os.getpgid(v['proc_handler'].pid), signal.SIGTERM)  # Send the signal to all the process groups
