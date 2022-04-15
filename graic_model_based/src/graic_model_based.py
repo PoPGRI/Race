1c162ab22fd6fc7ec2f820f76eb833c0a5bb21b5
Yan <you@example.com>
2022-04-14 22:11:35 -0500

    sync mode in model_based

--- graic_model_based/src/graic_model_based.py
+++ graic_model_based/src/graic_model_based.py
@@ -50,6 +50,7 @@ class ModelBasedVehicle:
         self.world = client.get_world()
         self.vehicle_dyn = VehicleDynamics()
         self.state = None
+        self.ready = False
         self.input = [0, 0]
         self.vehicle = None
         self.speed_control = PID(Kp=1.5,
@@ -82,6 +83,7 @@ class ModelBasedVehicle:
         self.vehicle.set_simulate_physics(False)
 
     def controlCallback(self, data):
+        self.ready = True
         self.vehicle_control_cmd = data
         self.computeInput()
 
@@ -105,6 +107,7 @@ class ModelBasedVehicle:
         self.input[0] = steer # FIXME
 
     def ackermannCallback(self, data):
+        self.ready = True
         self.speed_control.setpoint = data.speed
         force = self.speed_control(self.state[2])
         force_range = [-self.vehicle_dyn.brake_curve(1.), self.vehicle_dyn.throttle_curve(1.)]
@@ -141,12 +144,12 @@ class ModelBasedVehicle:
 def run(role_name, host, port):
     vehicle = ModelBasedVehicle(role_name, host, port)
 
-    freq = 20
-    rate = rospy.Rate(freq)
+    fixed_delta_seconds = 0.05
 
     while not rospy.is_shutdown():
-        vehicle.tick(1./freq)
-        rate.sleep()
+        if vehicle.ready:
+            vehicle.tick(fixed_delta_seconds)
+            vehicle.ready = False
 
 if __name__ == "__main__":
     rospy.init_node("graic_model_based")
