da2b315b9a5d66b4f515768a48e6be178f7afa4c
Yan <48262576+yanmiao2@users.noreply.github.com>
2023-10-12 13:34:39 -0500

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 import pickle 
 
-map = "shanghai_intl_circuit" # t1_triple, t2_triple, t3, t4, shanghai_intl_circuit
+map = "t3" # t1_triple, t2_triple, t3, t4, shanghai_intl_circuit
 
 controller_process = subprocess.Popen("python3 automatic_control_GRAIC.py --sync -m {}".format(map), shell=True)
 time.sleep(5)
